0720ea83d04d21c618655fd03e2e12f051e1d34c
zhsssy <893118851@qq.com>
2021-12-28 17:36:47 +0800

    :zap: 使用 shader / glsl uniform 设置颜色 || 不在着色器中进行绑定

--- fs.glsl
+++ fs.glsl
@@ -1,7 +1,7 @@
 #version 330
-in vec3 color;
+uniform vec3 setColor;
 
 void main()
 {
-	gl_FragColor = vec4(color, 0.5);
+    gl_FragColor = vec4(setColor, 0.5);
 }
--- vs.glsl
+++ vs.glsl
@@ -1,18 +1,12 @@
 #version 330
 
 layout(location = 0) in vec3 iPos;
-layout(location = 1) in vec3 iColor;
 
 uniform mat4 mMat;
 uniform mat4 vMat;
 uniform mat4 pMat;
 
-out vec3 color;
-
 void main()
 {
-	gl_Position = pMat * vMat * mMat * vec4(iPos, 1.0f);
-// 指定颜色
-// 灰色
-	color = iColor;
+    gl_Position = pMat * vMat * mMat * vec4(iPos, 1.0f);
 }
